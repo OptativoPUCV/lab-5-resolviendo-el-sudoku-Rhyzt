4182379890507958b39afd0af049699b34c5ca2e
Your Name <you@example.com>
2025-05-17 01:45:09 -0400

    new try

--- sudoku.c
+++ sudoku.c
@@ -114,7 +114,7 @@ List* get_adj_nodes(Node* n){
 
 int is_final(Node* n) {
     List *lista = get_adj_nodes(n);
-    if (lista == NULL) {
+    if (first(lista) == NULL) {
         for (int i = 0 ; i < 9 ; i++)
             for (int k = 0 ; k < 9 ; k++)   
                 if (n -> sudo[i][k] == 0) {
